91e839a46437cb2c079bfcafcdbbf0b99f123e4b
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2025-12-07 14:31:07 +0100

    day 7 faaast

--- day07/src/bin/part2.rs
+++ day07/src/bin/part2.rs
@@ -1,35 +1,51 @@
 use aoc2025::world::World;
 use glam::IVec2;
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 const DOWN: IVec2 = IVec2::new(0, 1);
 const LEFT: IVec2 = IVec2::new(-1, 0);
 const RIGHT: IVec2 = IVec2::new(1, 0);
 
-fn solve(input: &str) -> u64 {
-    let world = World::new(input);
+fn descend(position: IVec2, world: &World, cache: &mut HashMap::<IVec2, u64>) {
+    let mut count = 0;
 
-    let &initial = world.find_ivec2('S').first().unwrap();
-
-    let mut pending = VecDeque::from([initial]);
-    let mut timeline_count = 0;
+    let mut pending: VecDeque::<IVec2> = match world.get_at(position).unwrap() {
+        '^' => VecDeque::from([position + LEFT, position + RIGHT]),
+        'S' => VecDeque::from([position]),
+        _ => panic!("Unexpected square")
+    };
 
     while let Some(position) = pending.pop_front() {
         let next_pos = position + DOWN;
 
         match world.get_at(next_pos) {
-            Some('.') => {
-                pending.push_back(next_pos)
-            }
+            Some('.') => pending.push_back(next_pos),
             Some('^') => {
-                pending.push_back(next_pos + LEFT);
-                pending.push_back(next_pos + RIGHT);
+                count += cache.get(&next_pos).expect("Should have already computed")
             }
             Some(_) => panic!("Unknown tile"),
-            None => {timeline_count += 1}
+            None => count += 1,
         }
     }
-    timeline_count
+    cache.insert(position, count);
+}
+
+fn solve(input: &str) -> u64 {
+    let world = World::new(input);
+
+    let mut splitters = world.find_ivec2('^');
+    let start_pos = *world.find_ivec2('S').first().unwrap();
+
+    splitters.sort_by(|a, b| b.y.cmp(&a.y));
+    splitters.push(start_pos);
+
+    let mut timelines_per_splitter = HashMap::<IVec2, u64>::new();
+
+    for splitter in splitters {
+        descend(splitter, &world, &mut timelines_per_splitter)
+    }
+
+    *timelines_per_splitter.get(&start_pos).unwrap()
 }
 
 fn main() {
